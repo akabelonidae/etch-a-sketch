fcfdce2a648c5dbfd64f067a41a26d61ca6587e2
Oguz <aka@pop-os.localdomain>
2022-10-12 01:38:34 +0300

    deleted eraser button and added more css style. next is rgb generator

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const grid = document.querySelector('#grid');
 const createGrid = function () {
   for (let i = 0; i < 256; i++) {
     const gridFillers = document.createElement('div');
-    gridFillers.classList.add('gridPieces');
+    gridFillers.classList.add('gridFill');
     grid.appendChild(gridFillers);
 
+    // Change grid color
     gridFillers.addEventListener('mouseover', function () {
       gridFillers.style.backgroundColor =
         document.querySelector('#color-picker').value;
+      gridFillers.style.transition = 'all .25s';
 
       // Clear button
       document
