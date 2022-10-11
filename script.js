70c3e230627e1120ebccb4a4aaa01a72555ae5d4
Oguz <aka@pop-os.localdomain>
2022-10-11 21:24:25 +0300

    added clear button and changed js a little bit

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ const grid = document.querySelector('#grid');
 const createGrid = function () {
   for (let i = 0; i < 256; i++) {
     const gridFillers = document.createElement('div');
-    grid.appendChild(gridFillers);
     gridFillers.classList.add('gridPieces');
+    grid.appendChild(gridFillers);
 
     gridFillers.addEventListener('mouseover', function () {
       gridFillers.style.backgroundColor = 'black';
+      document.querySelector('#clear').addEventListener('click', function () {
+        gridFillers.style.backgroundColor = 'white';
+      });
     });
   }
 };
+
 createGrid();
