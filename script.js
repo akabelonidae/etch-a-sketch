f8112ba2381b8ecefab2aa027d6bffa93c3d31b8
Oguz <aka@pop-os.localdomain>
2022-10-13 16:55:39 +0300

    implemented rainbow mode and fixed color picking bug. rainbow is not fully implemented yet

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 // Variables
 const grid = document.querySelector('#grid');
 const gridSlider = document.querySelector('#grid-slider');
+const colorPicker = document.querySelector('#color-picker');
+const rainbowBtn = document.querySelector('.rgb-btn');
+let rainbowMode = 0;
+const startValues = function () {
+  createGrid(gridSlider.value);
+  gridColor(colorPicker.value);
+};
 
 // Create grid elements
 const createGrid = function (gridCount) {
@@ -13,17 +20,58 @@ const createGrid = function (gridCount) {
     grid.appendChild(newGridElements);
   }
 };
-createGrid(gridSlider.value);
 
-// Change grid counts with slider
+// Slider function
 gridSlider.addEventListener('click', function () {
-  const divAll = document.querySelectorAll('.newGrids');
-  divAll.forEach((div) => div.remove());
+  const gridsAll = document.querySelectorAll('.newGrids');
+  gridsAll.forEach((div) => div.remove());
 
-  // Create new grids again with value update
+  // Create new grids again with new value
   createGrid(gridSlider.value);
+  gridColor();
 
   document.querySelector(
     '#slider-text'
   ).textContent = `${gridSlider.value}x${gridSlider.value}`;
 });
+
+// Paint function
+const gridColor = function (color) {
+  const gridsAll = document.querySelectorAll('.newGrids');
+
+  gridsAll.forEach((grid) =>
+    grid.addEventListener('mouseover', function () {
+      grid.style.backgroundColor = color ? color : colorPicker.value;
+    })
+  );
+};
+
+// Random rgb generator
+const rgbGenerator = function () {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const rgb = `${r},${g},${b}`;
+  console.log(rgb);
+  return rgb;
+};
+
+// Rainbow button
+rainbowBtn.addEventListener('click', function () {
+  if (rainbowMode == 0) {
+    rgbGenerator();
+    rainbowBtn.classList.add('rainbowMode');
+    rainbowBtn.textContent = 'Rainbow Mode On!';
+    rainbowMode = 1;
+    gridColor('blue');
+  } else if (rainbowMode == 1) {
+    rainbowBtn.classList.remove('rainbowMode');
+    rainbowBtn.textContent = 'Rainbow Mode Off!';
+    rainbowMode = 0;
+    gridColor(colorPicker.value);
+  }
+});
+
+//
+startValues();
+gridColor();
