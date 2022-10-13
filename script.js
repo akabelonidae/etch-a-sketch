ff4a3daeb78c70cc4973f8027daaad9495f50a2a
Oguz <aka@pop-os.localdomain>
2022-10-13 18:05:29 +0300

    added rainbow function. idk i doubt it is the best or cleanest solution. i had not help from internet

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ const gridSlider = document.querySelector('#grid-slider');
 const colorPicker = document.querySelector('#color-picker');
 const rainbowBtn = document.querySelector('.rgb-btn');
 let rainbowMode = 0;
-const startValues = function () {
-  createGrid(gridSlider.value);
-  gridColor(colorPicker.value);
-};
 
 // Create grid elements
 const createGrid = function (gridCount) {
@@ -21,49 +17,54 @@ const createGrid = function (gridCount) {
   }
 };
 
-// Slider function
+// Grid x Grid slider
 gridSlider.addEventListener('click', function () {
   const gridsAll = document.querySelectorAll('.newGrids');
   gridsAll.forEach((div) => div.remove());
 
-  // Create new grids again with new value
   createGrid(gridSlider.value);
-  gridColor();
+  gridColor(colorPicker.value);
 
   document.querySelector(
     '#slider-text'
   ).textContent = `${gridSlider.value}x${gridSlider.value}`;
 });
 
-// Paint function
-const gridColor = function (color) {
+// Grid color function
+const gridColor = function () {
   const gridsAll = document.querySelectorAll('.newGrids');
 
   gridsAll.forEach((grid) =>
     grid.addEventListener('mouseover', function () {
-      grid.style.backgroundColor = color ? color : colorPicker.value;
+      grid.style.backgroundColor = colorPicker.value;
     })
   );
 };
 
-// Random rgb generator
-const rgbGenerator = function () {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  const rgb = `${r},${g},${b}`;
-  console.log(rgb);
-  return rgb;
+// Rainbow colors
+const rainbowColors = function () {
+  const gridsAll = document.querySelectorAll('.newGrids');
+
+  gridsAll.forEach((grid) =>
+    grid.addEventListener('mouseover', function () {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      grid.style.backgroundColor = `rgb(${r},${g},${b})`;
+    })
+  );
 };
 
 // Rainbow button
 rainbowBtn.addEventListener('click', function () {
+  // On
   if (rainbowMode == 0) {
-    rgbGenerator();
     rainbowBtn.classList.add('rainbowMode');
     rainbowBtn.textContent = 'Rainbow Mode On!';
     rainbowMode = 1;
-    gridColor('blue');
+    rainbowColors();
+
+    // Off
   } else if (rainbowMode == 1) {
     rainbowBtn.classList.remove('rainbowMode');
     rainbowBtn.textContent = 'Rainbow Mode Off!';
@@ -73,5 +74,5 @@ rainbowBtn.addEventListener('click', function () {
 });
 
 //
-startValues();
-gridColor();
+createGrid(gridSlider.value);
+gridColor(colorPicker.value);
