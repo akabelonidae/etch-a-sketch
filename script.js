8c4b42360e83a17654b104f2fcb268dc6cf2f2d2
Oguz <aka@pop-os.localdomain>
2022-10-13 14:49:52 +0300

    rewriting whole js section.so far i added create and update grid functionality. also added new button effects and grid slider

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 'use strict';
+// Variables
 const grid = document.querySelector('#grid');
+const gridSlider = document.querySelector('#grid-slider');
 
-// Create grid
-const createGrid = function () {
-  for (let i = 0; i < 256; i++) {
-    const gridFillers = document.createElement('div');
-    gridFillers.classList.add('gridFill');
-    grid.appendChild(gridFillers);
-
-    // Change grid color
-    gridFillers.addEventListener('mouseover', function () {
-      gridFillers.style.backgroundColor =
-        document.querySelector('#color-picker').value;
-      gridFillers.style.transition = 'all .25s';
-
-      // Clear button
-      document
-        .querySelector('.clear-btn')
-        .addEventListener('click', function () {
-          gridFillers.style.backgroundColor = 'white';
-        });
-    });
+// Create grid elements
+const createGrid = function (gridCount) {
+  for (let i = 0; i < gridCount * gridCount; i++) {
+    const newGridElements = document.createElement('div');
+    newGridElements.classList.add('newGrids');
+    newGridElements.style.height = `${960 / gridCount}px`;
+    newGridElements.style.width = `${960 / gridCount}px`;
+    grid.appendChild(newGridElements);
   }
 };
+createGrid(gridSlider.value);
 
-createGrid();
+// Change grid counts with slider
+gridSlider.addEventListener('click', function () {
+  const divAll = document.querySelectorAll('.newGrids');
+  divAll.forEach((div) => div.remove());
+
+  // Create new grids again with value update
+  createGrid(gridSlider.value);
+
+  document.querySelector(
+    '#slider-text'
+  ).textContent = `${gridSlider.value}x${gridSlider.value}`;
+});
